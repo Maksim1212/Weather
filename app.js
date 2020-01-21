f8092c21d90a97ff11b386679ed9f66986e20b4e
maksim1212 <mr.frost201611@gmail.com>
2020-01-21 21:46:41 +0200

    refactor app.js add days

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
 const PORT = '8888';
-
 const apiKey = 'c3e58021321d178c2b55d2533301f39b';
+let weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,9 +24,10 @@ app.get('/', function(req, res) {
 app.post('/', function(req, res) {
     let city = req.body.city;
     let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}
-                &units=imperial&appid=${apiKey}`
+    &units=imperial&appid=${apiKey}`
 
     request(url, function(err, response, body) {
+
         if (err) {
             res.render('index', { weather: null, error: 'Error, please try again' });
         } else {
@@ -27,13 +35,13 @@ app.post('/', function(req, res) {
             if (data == undefined) {
                 res.render('index', { weather: null, error: 'Error, please try again' });
             } else {
-                let weatherText = `It's id ${data.city.id} 
+                let weatherText = `It's id ${data.name} 
                  ${data.list[0].weather[0].icon}
-                  ${data.list[5].weather[0].description}
-                  ${new Date(data.list[0].dt * 1000)}
-                `;
-                res.render('index', { weather: weatherText, error: null });
+                  ${(new Date(data.list[0].dt * 1000)).getDay()}`;
 
+                //let numberDay = `${(new Date(data.list[0].dt * 1000)).getDay()}`;
+                //let dayName = weekday[numberDay];
+                res.render('index', { weather: weatherText, error: null });
             }
         }
     });
