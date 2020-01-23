bc02bb63bef4bdb100e20d6d640961074e25e969
maksim1212 <mr.frost201611@gmail.com>
2020-01-23 22:31:25 +0200

    forming a link to the picture and the names of days

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const request = require('request');
 const app = express()
 const PORT = '8888';
 const apiKey = 'c3e58021321d178c2b55d2533301f39b';
+const imageUri = 'https://openweathermap.org/img/wn/';
+const imageFormat = '@2x.png';
 let weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+weekday[0] = "Sun";
+weekday[1] = "Mon";
+weekday[2] = "Tue";
+weekday[3] = "Wed";
+weekday[4] = "Thu";
+weekday[5] = "Fri";
+weekday[6] = "Sat";
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -45,16 +47,17 @@ app.post('/', function(req, res) {
                 let dayKeys = Object.keys(unikDays);
 
                 for (let k = 0; k < dayKeys.length; k++) {
-                    console.log(new Date(unikDays[dayKeys[k]].dt * 1000).getDay());
-                    console.log(unikDays[dayKeys[k]].weather[0].icon);
+                    let numberDay = `${new Date(unikDays[dayKeys[k]].dt * 1000).getDay()}`;
+                    dayName = weekday[numberDay];
+                    weatherImage = `${imageUri}${unikDays[dayKeys[k]].weather[0].icon}${imageFormat}`;
+
+                    console.log(dayName);
+                    console.log(weatherImage);
                 }
 
-                // console.log(new Date(unikDays[23].dt * 1000).getDay());
-                // console.log(unikDays[23].weather[0].icon);
-
-                let weatherText = `It's id ${data.name} 
-                 ${data.list[1].weather[0].icon}
-                  ${(new Date(data.list[0].dt * 1000)).getDay()}`;
+                // let weatherText = `It's id ${data.name} 
+                //  ${data.list[1].weather[0].icon}
+                //   ${(new Date(data.list[0].dt * 1000)).getDay()}`;
 
                 //let numberDay = `${(new Date(data.list[0].dt * 1000)).getDay()}`;
                 //let dayName = weekday[numberDay];
