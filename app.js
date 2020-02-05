8d06200c573c998497af0cb29b82301e7c919a1b
maksim1212 <mr.frost201611@gmail.com>
2020-02-05 23:19:36 +0200

    add gitignore and some fix codestyle

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const app = express()
+
+const app = express();
 const PORT = '8888';
 const apiKey = 'c3e58021321d178c2b55d2533301f39b';
 const imageUri = 'https://openweathermap.org/img/wn/';
 const imageFormat = '@2x.png';
-let weekday = new Array(7);
-weekday[0] = "Sun";
-weekday[1] = "Mon";
-weekday[2] = "Tue";
-weekday[3] = "Wed";
-weekday[4] = "Thu";
-weekday[5] = "Fri";
-weekday[6] = "Sat";
+const weekday = new Array(7);
+weekday[0] = 'Sun';
+weekday[1] = 'Mon';
+weekday[2] = 'Tue';
+weekday[3] = 'Wed';
+weekday[4] = 'Thu';
+weekday[5] = 'Fri';
+weekday[6] = 'Sat';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.render('index.ejs', { weather: null, error: null });
-})
+});
 
 app.post('/', function(req, res) {
-    let city = req.body.city;
-    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}
+    const { city } = req.body;
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}
     &units=imperial&appid=${apiKey}`
 
     request(url, function(err, response, body) {
@@ -33,38 +34,38 @@ app.post('/', function(req, res) {
         if (err) {
             res.render('index', { weather: null, error: 'Error, please try again' });
         } else {
-            let data = JSON.parse(body) //parse JSON data
-            if (data == undefined || data.cod !== '200') {
+            const data = JSON.parse(body) // parse JSON data
+            if (data === undefined || data.cod !== '200') {
                 res.render('index', { weather: null, error: 'Error, please try again' });
             } else {
-                const unikDays = {}; //object with sorted days
+                const unikDays = {}; // object with sorted days
                 for (let i = 0; i < data.list.length; i++) {
                     const date = new Date(data.list[i].dt_txt);
                     if (!unikDays[date.getDate()]) {
-                        unikDays[date.getDate()] = data.list[i] //sorted days
+                        unikDays[date.getDate()] = data.list[i] // sorted days
                     }
                 }
 
-                let dayKeys = Object.keys(unikDays);
+                const dayKeys = Object.keys(unikDays);
                 const dayNameObj = {};
                 const weatherImageObj = {};
                 for (let k = 0; k < dayKeys.length; k++) {
-                    let numberDay = `${new Date(unikDays[dayKeys[k]].dt * 1000).getDay()}`; //get the day number from unix format
-                    dayNameObj[k] = weekday[numberDay]; //give the numbers of the day names
-                    weatherImageObj[k] = `${imageUri}${unikDays[dayKeys[k]].weather[0].icon}${imageFormat}`; //form the address of the picture
+                    const numberDay = `${new Date(unikDays[dayKeys[k]].dt * 1000).getDay()}`; // get the day number from unix format
+                    dayNameObj[k] = weekday[numberDay]; // give the numbers of the day names
+                    weatherImageObj[k] = `${imageUri}${unikDays[dayKeys[k]].weather[0].icon}${imageFormat}`; // form the address of the picture
                 }
 
-                let dayName = Object.values(dayNameObj); //take values ​​from the object with the names of the days
-                let weatherImage = Object.values(weatherImageObj); //take values ​​from the object with the images uri
+                const dayName = Object.values(dayNameObj); // take values from the object with the names of the days
+                const weatherImage = Object.values(weatherImageObj); // take values from the object with the images uri
 
                 console.log(dayName); // log result day name 
                 console.log(weatherImage); // log image address
 
-                res.render('index', { weather: dayName, weatherImage, error: null }); //send data to view
+                res.render('index', { weather: dayName, weatherImage, error: null }); // send data to view
             }
         }
     });
-})
+});
 
 app.listen(PORT, function() {
     console.log('Weather app listening on port 8888')
